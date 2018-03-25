8d538849dba79fed89145e1ffba318c20a664644
Anish Nag <anishnag00@gmail.com>
2018-03-24 17:35:47 -0700

    comment incomplete code

--- key.py
+++ key.py
@@ -14,11 +14,12 @@ def on_release(key):
 	if key == keyboard.Key.esc: return False
 
 	i = len(keylog)-1
-	while keylog[i][1] 
+        # remove duplicated key pressed data
+        # while keylog[i][1]
 
 
 	for i in reversed(xrange(len(keylog))):
-		keylog[i][1] 
+		keylog[i][1]
 	keylog.append((key, 'release', time))
 
 with keyboard.Listener(on_press=on_press, on_release=on_release) as listener:
