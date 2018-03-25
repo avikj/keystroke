140dd47cf65ec93ab230ed84191062427357f2d8
Anish Nag <anishnag00@gmail.com>
2018-03-24 17:34:21 -0700

    initial key logger

---
+++ key.py
@@ -0,0 +1,25 @@
+from pynput import keyboard
+import datetime
+
+keylog = []
+
+def on_press(key):
+	time = (datetime.datetime.now()-datetime.datetime(1970,1,1)).total_seconds()
+	try: keychar = key.char
+	except AttributeError: keychar = key
+	keylog.append((keychar, 'press', time))
+
+def on_release(key):
+	time = (datetime.datetime.now()-datetime.datetime(1970,1,1)).total_seconds()
+	if key == keyboard.Key.esc: return False
+
+	i = len(keylog)-1
+	while keylog[i][1] 
+
+
+	for i in reversed(xrange(len(keylog))):
+		keylog[i][1] 
+	keylog.append((key, 'release', time))
+
+with keyboard.Listener(on_press=on_press, on_release=on_release) as listener:
+	listener.join()
