cd98be52eae0e1d2ebf20e56505fc8df8928b0d5
Avik Jain <avikjain12345@gmail.com>
2018-03-24 20:42:03 -0700

    updates

--- key.py
+++ key.py
@@ -1,26 +1,29 @@
 from pynput import keyboard
 import datetime
+from copy import deepcopy
+class Keyshit:
+	def __init__(self):
+		self.keylog = []
+		self.PRESSED = True
+		self.RELEASED = False
+		self.logs = []
+		def on_press(key):
+			time = (datetime.datetime.now()-datetime.datetime(1970,1,1)).total_seconds()
+			try: keychar = key.char
+			except AttributeError: keychar = key
+			if len(self.keylog) == 0 or not (self.keylog[-1][0] == keychar and self.keylog[-1][1] == self.PRESSED): # filter out repeated self.PRESSED signals
+				self.keylog.append((keychar, self.PRESSED, time))
+				print(keychar, self.PRESSED, time)
+			if keychar in [keyboard.Key.space, keyboard.Key.enter, keyboard.Key.tab]:
+				print 'RESET'
+				self.logs.append(deepcopy(self.keylog))
+				self.keylog = []
+		def on_release(key):
+			time = (datetime.datetime.now()-datetime.datetime(1970,1,1)).total_seconds()
+			if key == keyboard.Key.esc: return False
+			self.keylog.append((key, self.RELEASED, time))
+			print (key, self.RELEASED, time)
+		with keyboard.Listener(on_press=on_press, on_release=None) as listener:
+			listener.join()
 
-keylog = []
-
-def on_press(key):
-	time = (datetime.datetime.now()-datetime.datetime(1970,1,1)).total_seconds()
-	try: keychar = key.char
-	except AttributeError: keychar = key
-	keylog.append((keychar, 'press', time))
-
-def on_release(key):
-	time = (datetime.datetime.now()-datetime.datetime(1970,1,1)).total_seconds()
-	if key == keyboard.Key.esc: return False
-
-	i = len(keylog)-1
-        # remove duplicated key pressed data
-        # while keylog[i][1]
-
-
-	for i in reversed(xrange(len(keylog))):
-		keylog[i][1]
-	keylog.append((key, 'release', time))
-
-with keyboard.Listener(on_press=on_press, on_release=on_release) as listener:
-	listener.join()
+Keyshit()
