4051710c367934a68789c732826e2b364fda8376
Shaurya Srivastava <shauryas@Shauryas-MacBook-Pro.local>
2018-03-24 21:35:46 -0700

    create dictionary from list of common words

--- dictionary.py
+++ dictionary.py
@@ -1,17 +1,23 @@
-from pynput import keyboard
-import datetime
-from os import path
-import os
+
+dictionary = {}
 
 def main():
-	#openFile()
-	#collectData()
-	with keyboard.Listener(on_press=on_press, on_release=on_release) as listener:
-		listener.join()
+	createDict()
+	#test()
 
-def openFile():
+def createDict():
 	filepath = 'CommonWords.txt'  
 	with open(filepath) as fp:  
 		for cnt, line in enumerate(fp):
-  			testWords.append(line) 
+  			dictionary[line[:-1]] = 0.0
 
+def test():
+	print len(dictionary)
+	for key in dictionary.keys():
+		print key
+	print dictionary["the"]
+	print dictionary["of"]
+	print dictionary["a"]
+	print dictionary["stuff"]
+
+if __name__ == '__main__': main()
