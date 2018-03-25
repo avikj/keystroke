1d2e02f1edf7308d252b84651596f075b75369f9
Shaurya Srivastava <shauryas@Shauryas-MacBook-Pro.local>
2018-03-24 23:42:18 -0700

    dictionary fuctionality

--- dictionary.py
+++ dictionary.py
@@ -1,23 +1,33 @@
+#all the 0.0 will be replaced with the times for the word from the algorithm
+#addWord() will be called when the algorithm creates a new word
 
-dictionary = {}
+import pickle
+import os.path
+
+
+personalDictionary = {}
 
 def main():
 	createDict()
+	addWord("abcd")
 	#test()
 
 def createDict():
-	filepath = 'CommonWords.txt'  
-	with open(filepath) as fp:  
-		for cnt, line in enumerate(fp):
-  			dictionary[line[:-1]] = 0.0
+	print os.path.exists("PersonalDictionary.pickle")
+	if os.path.exists("PersonalDictionary.pickle"):
+		with open('PersonalDictionary.pickle', 'rb') as f:  
+			global personalDictionary
+			personalDictionary = pickle.load(f)
+			print personalDictionary
 
-def test():
-	print len(dictionary)
-	for key in dictionary.keys():
-		print key
-	print dictionary["the"]
-	print dictionary["of"]
-	print dictionary["a"]
-	print dictionary["stuff"]
+
+def addWord(word):
+	global personalDictionary
+	print personalDictionary
+	if personalDictionary.has_key(word)==False:
+		personalDictionary[word] = 0.0
+		print personalDictionary
+	with open('PersonalDictionary.pickle', 'wb') as f:
+		pickle.dump(personalDictionary, f)
 
 if __name__ == '__main__': main()
